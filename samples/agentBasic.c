4510ccb45d0602b963f7ce04495d3e869f63b2f3
Gracjan Orzechowski <orzechowski.gracjan@gmail.com>
2020-12-03 18:32:38 +0100

    Adjusted basic agent sample

--- samples/agentBasic.c
+++ samples/agentBasic.c
@@ -3,20 +3,40 @@
 #include <memory.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 #include "veides_agent_client.h"
 
+#define MAX_LOG_BUFSIZE 8192
+
 volatile int finish = 0;
+
+void log_message(void *target, const char *items, ...) {
+    va_list args;
+
+    char buffer[MAX_LOG_BUFSIZE];
+    memset(buffer, '0', MAX_LOG_BUFSIZE);
+
+    va_start(args, items);
+    vsnprintf(buffer, MAX_LOG_BUFSIZE, items, args);
+    va_end(args);
+
+    fprintf(stdout, "Logging %s", buffer);
+    fflush(stdout);
+
+    fprintf(target, "%s\n", buffer);
+    fflush(target);
+}
+
+void sdkLogCallback(int level, char *message) {
+    fprintf(stdout, "%s", message);
+    fflush(stdout);
+}
 
 void sigHandler(int signalNumber) {
     signal(SIGINT, NULL);
-    fprintf(stdout, "Received signal: %d\n", signalNumber);
+    log_message(stdout, "INFO: Received signal: %d", signalNumber);
     finish = 1;
-}
-
-void logCallback(int level, char *message) {
-    fprintf(stdout, "%s", message);
-    fflush(stdout);
 }
 
 void printHelp() {
@@ -35,36 +55,71 @@ void printHelp() {
     );
 }
 
+int sendInitialFacts(VeidesAgentClient *agent) {
+    VEIDES_RC rc = VEIDES_RC_SUCCESS;
+
+    // Create facts object
+    VeidesFacts *facts = NULL;
+    rc = VeidesAgentClientFacts_create(&facts);
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "ERROR: Failed to initialize Veides Facts (rc=%d)", rc);
+        return rc;
+    }
+
+    // Add initial fact
+    rc = VeidesAgentClientFacts_addFact(facts, "battery_level", "full");
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "ERROR: Failed to add fact to facts array (rc=%d)", rc);
+        return rc;
+    }
+
+    rc = VeidesAgentClientFacts_addFact(facts, "charging", "no");
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "ERROR: Failed to add fact to facts array (rc=%d)", rc);
+        return rc;
+    }
+
+    // Send initial facts
+    rc = VeidesAgentClient_sendFacts(agent, facts);
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "ERROR: Failed to send facts (rc=%d)", rc);
+        return rc;
+    }
+
+    // Clear facts array
+    VeidesAgentClientFacts_destroy(facts);
+
+    return rc;
+}
+
 void printActionEntities(VeidesActionEntity *entities, size_t entitieslen) {
     for (int i = 0; i < entitieslen; i++) {
         if (entities[i].valueType == VEIDES_ENTITY_VALUE_STRING) {
-            fprintf(stdout, "%d entity is: name=%s, value=%s\n", i, entities[i].name, entities[i].valueString);
+            log_message(stdout, "INFO: %d entity is: name=%s, value=%s", i, entities[i].name, entities[i].valueString);
         } else if (entities[i].valueType == VEIDES_ENTITY_VALUE_INT) {
-            fprintf(stdout, "%d entity is: name=%s, value=%d\n", i, entities[i].name, entities[i].valueInt);
+            log_message(stdout, "INFO: %d entity is: name=%s, value=%d", i, entities[i].name, entities[i].valueInt);
         } else if (entities[i].valueType == VEIDES_ENTITY_VALUE_DOUBLE) {
-            fprintf(stdout, "%d entity is: name=%s, value=%f\n", i, entities[i].name, entities[i].valueDouble);
+            log_message(stdout, "INFO: %d entity is: name=%s, value=%f", i, entities[i].name, entities[i].valueDouble);
         } else if (entities[i].valueType == VEIDES_ENTITY_VALUE_BOOL) {
-            fprintf(stdout, "%d entity is: name=%s, value=%d\n", i, entities[i].name, entities[i].valueBool);
+            log_message(stdout, "INFO: %d entity is: name=%s, value=%d", i, entities[i].name, entities[i].valueBool);
         }
     }
 }
 
-void onAnyAction(VeidesAgentClient *agent, char *name, VeidesActionEntity *entities, size_t entitieslen) {
-    fprintf(stdout, "[onAnyAction] Action received: %s\n", name ? name : "NULL");
+// void onAnyAction(VeidesAgentClient *agent, char *name, VeidesActionEntity *entities, size_t entitieslen) {
+//     log_message(stdout, "INFO: Action received: %s", name ? name : "NULL");
+
+//     printActionEntities(entities, entitieslen);
+
+//     VeidesAgentClient_sendActionCompleted(agent, name);
+// }
+
+void onSetLowSpeedAction(VeidesAgentClient *agent, VeidesActionEntity *entities, size_t entitieslen) {
+    log_message(stdout, "INFO: Action received: %s", "set_low_speed");
 
     printActionEntities(entities, entitieslen);
 
-    fflush(stdout);
-
-    VeidesAgentClient_sendActionCompleted(agent, name);
-}
-
-void onAction(VeidesAgentClient *agent, VeidesActionEntity *entities, size_t entitieslen) {
-    fprintf(stdout, "[onAction] Action received: %s\n", "set_low_speed");
-
-    printActionEntities(entities, entitieslen);
-
-    fflush(stdout);
+    VeidesAgentClient_sendTrail(agent, "speed_level", "low");
 
     VeidesAgentClient_sendActionCompleted(agent, "set_low_speed");
 }
@@ -97,18 +152,17 @@ int main(int argc, char *argv[]) {
     signal(SIGTERM, sigHandler);
 
     // Set SDK log handler
-    rc = VeidesAgentClient_setLogHandler(&logCallback);
+    rc = VeidesAgentClient_setLogHandler(&sdkLogCallback);
     if (rc != VEIDES_RC_SUCCESS) {
-        fprintf(stderr, "WARN: Failed to set SDK log handler (rc=%d)\n", rc);
-        exit(1);
+        log_message(stderr, "WARN: Failed to set SDK log handler (rc=%d)", rc);
     }
 
-    // Use VeidesAgentClient_setLogLevel() to change SDk log level
-    // VeidesAgentClient_setLogLevel(VEIDES_LOG_DEBUG);
+    // Set DEBUG level to see received and sent data. Level is INFO by default
+    VeidesAgentClient_setLogLevel(VEIDES_LOG_DEBUG);
 
     rc = VeidesAgentClientProperties_create(&properties);
     if (rc != VEIDES_RC_SUCCESS) {
-        fprintf(stderr, "ERROR: Failed to initialize properties (rc=%d)\n", rc);
+        log_message(stderr, "ERROR: Failed to initialize properties (rc=%d)", rc);
         exit(1);
     }
 
@@ -123,67 +177,93 @@ int main(int argc, char *argv[]) {
 
     rc = VeidesAgentClient_create(&agent, properties);
     if (rc != VEIDES_RC_SUCCESS) {
-        fprintf(stderr, "ERROR: Failed to initialize Veides Agent client (rc=%d)\n", rc);
+        log_message(stderr, "ERROR: Failed to initialize Veides Agent client (rc=%d)", rc);
         exit(1);
     }
 
     rc = VeidesAgentClient_connect(agent);
     if (rc != VEIDES_RC_SUCCESS) {
-        fprintf(stderr, "ERROR: Failed to connect to Veides (rc=%d)\n", rc);
+        log_message(stderr, "ERROR: Failed to connect to Veides (rc=%d)", rc);
         exit(1);
     }
 
-    // Set a handler for any action received
+    // Set a handler for particular action
+    VeidesAgentClient_setActionHandler(agent, "set_low_speed", onSetLowSpeedAction);
+
+    // You can also set a handler for any action received.
+    // It will execute when there's no callback set for the particular action
     // VeidesAgentClient_setAnyActionHandler(agent, onAnyAction);
 
-    // Set a handler for particular action
-    // VeidesAgentClient_setActionHandler(agent, "set_low_speed", onAction);
-
-    // Create facts object
-    // VeidesFacts *facts = NULL;
-    // rc = VeidesAgentClientFacts_create(&facts);
-    // if (rc != VEIDES_RC_SUCCESS) {
-        // fprintf(stderr, "ERROR: Failed to initialize Veides Facts (rc=%d)\n", rc);
-    // }
-
-    // Add initial fact
-    // rc = VeidesAgentClientFacts_addFact(facts, "battery_level", "critical");
-    // if (rc != VEIDES_RC_SUCCESS) {
-        // fprintf(stderr, "ERROR: Failed to add fact to facts array (rc=%d)\n", rc);
-    // }
-
     // Send initial facts
-    // rc = VeidesAgentClient_sendFacts(agent, facts);
-    // if (rc != VEIDES_RC_SUCCESS) {
-        // fprintf(stderr, "ERROR: Failed to send facts (rc=%d)\n", rc);
-    // }
+    rc = sendInitialFacts(agent);
+    if (rc != VEIDES_RC_SUCCESS) {
+        exit(1);
+    }
 
-    // Clear facts array
-    // VeidesAgentClientFacts_destroy(facts);
+    // Send a trail
+    rc = VeidesAgentClient_sendTrail(agent, "speed_level", "normal");
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "WARN: Failed to send trail (rc=%d)", rc);
+    }
 
     // Send ready event
-    // rc = VeidesAgentClient_sendEvent(agent, "ready_to_rock");
-    // if (rc != VEIDES_RC_SUCCESS) {
-        // fprintf(stderr, "ERROR: Failed to send an event (rc=%d)\n", rc);
-        // exit(1);
-    // }
-
-    int upTime = 0;
-
-    while(!finish) {
-        char trailValue[100];
-        sprintf(trailValue, "%d", upTime);
-
-        // Send example trail (to see the value in the Console you need to setup dashboard first)
-        // rc = VeidesAgentClient_sendTrail(agent, "up_time", trailValue);
-
-        sleep(1);
-        upTime++;
+    rc = VeidesAgentClient_sendEvent(agent, "ready_to_rock");
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "WARN: Failed to send an event (rc=%d)", rc);
     }
+
+    int uptime = 0;
+
+    while (!finish) {
+        sleep(1);
+        uptime++;
+
+        char trailValue[100];
+        sprintf(trailValue, "%d", uptime);
+
+        // Send trail
+        rc = VeidesAgentClient_sendTrail(agent, "uptime", trailValue);
+        if (rc != VEIDES_RC_SUCCESS) {
+            log_message(stderr, "WARN: Failed to send trail (rc=%d)", rc);
+        }
+
+        if (uptime == 10) {
+            VeidesFacts *facts = NULL;
+
+            VeidesAgentClientFacts_create(&facts);
+            VeidesAgentClientFacts_addFact(facts, "battery_level", "low");
+            // Send facts update
+            rc = VeidesAgentClient_sendFacts(agent, facts);
+            if (rc != VEIDES_RC_SUCCESS) {
+                log_message(stderr, "WARN: Failed to send facts (rc=%d)", rc);
+            }
+            VeidesAgentClientFacts_destroy(facts);
+        }
+    }
+
+    VeidesFacts *facts = NULL;
+
+    rc = VeidesAgentClientFacts_create(&facts);
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "WARN: Failed to initialize Veides Facts (rc=%d)", rc);
+    }
+
+    rc = VeidesAgentClientFacts_addFact(facts, "battery_level", "unknown");
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "WARN: Failed to add fact to facts array (rc=%d)", rc);
+    }
+
+    // Send facts update
+    rc = VeidesAgentClient_sendFacts(agent, facts);
+    if (rc != VEIDES_RC_SUCCESS) {
+        log_message(stderr, "WARN: Failed to send facts (rc=%d)", rc);
+    }
+
+    VeidesAgentClientFacts_destroy(facts);
 
     rc = VeidesAgentClient_disconnect(agent);
     if (rc != VEIDES_RC_SUCCESS) {
-        fprintf(stderr, "ERROR: Failed to disconnect from Veides (rc=%d)\n", rc);
+        log_message(stderr, "ERROR: Failed to disconnect from Veides (rc=%d)", rc);
         exit(1);
     }
 
